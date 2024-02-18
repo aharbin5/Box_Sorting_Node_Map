8bc839f909851d65df43ae58af4d67449d8a1fb4
aharbin5 <aharbin5@students.kennesaw.edu>
2024-02-18 00:30:28 -0500

    basic json config setup

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,54 @@ mod structs;
 use std::net::TcpListener;
 use std::io::Read;
 use local_ip_address::local_ip;
+use serde_json::Value;
+
+fn get_config() -> serde_json::Value {
+    let my_local_ip_address: String = "127.0.0.1".to_string();
+    match local_ip()
+    {
+        Ok(t) => {let my_local_ip_address = t.to_string(); println!("Local address: {}", my_local_ip_address)},
+        Err(_e) => {println!("Couldn't get ip address correctly, resorting to loopback");}
+    }
+    
+    let mut rx: String = "".to_string();
+
+    match TcpListener::bind("127.0.0.1".to_string() + ":48753")
+    {
+        Ok(t) => {
+            println!("Address binded, waiting for connection");
+            for stream in t.incoming()
+            {
+                match stream
+                {
+                    Ok(mut stream) => {
+                        match stream.read_to_string( &mut rx) // read_to_string could be used with a json parser or changed to read which gives a byte array
+                        {
+                            Ok(_t) => {println!("{}", rx)},
+                            Err(e) => println!("Error occured at reading packet to buffer\n{}", e)
+                        }
+                        break;
+                    },
+                    Err(e) => println!("Error occured at match incoming\n{}", e)
+                }
+            }
+        },
+        Err(e) => println!("Error occured at TCP Bind\n{}", e)
+    }
+
+    match serde_json::from_str(&rx)
+    {
+        Ok(t) => {return t},
+        Err(e) => {println!("{}", e); serde_json::from_str("fuck").unwrap()} // Semi-hack fix but it SHOULDN'T ever error.  Mint
+    }
+}
 
 fn main() {
     // Real package Vector
     let mut packages: Vec<structs::BoxStruct> = vec![];
 
     {   //////////////////////////////// Setup package list ////////////////////////////////
-        // xpos 0 .. 1024
-        // ypos 0 .. 1024
-        // tracking number 100 .. 110
-        // destination 1 .. 9
+        // xpos 0 .. 1024, ypos 0 .. 1024, tracking number 100 .. 110, destination 1 .. 9
         let temp_packages = [
             [0, 10, 100, 5],
             [100, 50, 101, 1],
@@ -86,41 +124,11 @@ fn main() {
 
     let route = [1,3,2,4,5,4,6,7,8,9,10];
     
-    // Wait for packet on port
-    let mut rx: String = "".to_string();
+    // Get config JSON from network and apply settings
+    println!("{}", get_config()["boxes"][2]);
 
-    let my_local_ip_address: String = "127.0.0.1".to_string();
-    match local_ip()
-    {
-        Ok(t) => {let my_local_ip_address = t.to_string(); println!("Local address: {}", my_local_ip_address)},
-        Err(_e) => {println!("Couldn't get ip address correctly, resorting to loopback");}
-    }
-
-    match TcpListener::bind(my_local_ip_address + ":48753")
-    {
-        Ok(t) => {
-            println!("Address binded, waiting for connection");
-            for stream in t.incoming()
-            {
-                match stream
-                {
-                    Ok(mut stream) => {
-                        match stream.read_to_string(&mut rx) // read_to_string could be used with a json parser or changed to read which gives a byte array
-                        {
-                            Ok(t) => {println!("{}", t); println!("{}", rx)},
-                            Err(e) => println!("Error occured at reading packet to buffer\n{}", e)
-                        }
-                        break;
-                    },
-                    Err(e) => println!("Error occured at match incoming\n{}", e)
-                }
-            }
-        },
-        Err(e) => println!("Error occured at TCP Bind\n{}", e)
-    }
 }
 
 // todo
-// 1. Make fn get_config() that does the whole TCP connect then JSON parses it
-// 2. Make fn route_sort() that takes in the BoxStructs on the shelves and assigns them places on the shelves
-// 3. Make async fn move_x and async fn move_y to simulate motor control
+// 1. Make fn route_sort() that takes in the BoxStructs on the shelves and assigns them places on the shelves
+// 2. Make async fn move_x and async fn move_y to simulate motor control
