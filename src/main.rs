9729b49f24e94366a2cffacc7102b29c53ec7e0d
aharbin5 <102874910+aharbin5@users.noreply.github.com>
2024-03-07 09:48:29 -0500

    Fixed packet config
    Removed excess code
    Fixed packet config
    I'm never adding the python file

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,6 @@ fn get_config() -> serde_json::Value {
 
 fn main() {
     // Get config from network
-    //let rx_config: Value = get_config();
     let rx_config: Value = serde_json::from_str(r#"
     {"route": [
         {"destination_id": 201, "x": 0, "y": 0}, 
@@ -64,6 +63,8 @@ fn main() {
         {"tracking_number": 107, "destination": 204},
         {"tracking_number": 108, "destination": 204},
         {"tracking_number": 109, "destination": 205}]}"#).unwrap();
+
+    let rx_config: Value = get_config();
 
     // Create Package Vector
     let mut packages: Vec<structs::BoxStruct> = vec![];
@@ -143,17 +144,7 @@ fn main() {
                 y_pos: i as u32 * 2
             });
         }
-
-        // Get distance between boxes and estimate time to move
-        // 44 inches is the worst distance it would have to move divided by an estimated 3.5 in/s
-        // multiplied by the number of boxes needing to be moved
-        let time_taken: f64 = (44.0 / 1.0) * packages.len() as f64;
-
-        println!("Total time taken: {}s", time_taken);
-        // Estimate distances between stops and fine enough time
     }
-
-    // Speem: 44inch diagonal at 3.5in/s
 
     // Prints package tracking numbers to verify loading into packages vector
     for i in 0 .. packages.len()
@@ -180,7 +171,7 @@ todo
  
 JSON:
 {
-    "route": [{x, y}, {x, y}, {x, y}, ...],
+    "route": [{"destination_id": 20x,"x": x, "y": y}, {"destination_id": 20x,"x": x, "y": y}, ...],
     "box_loc": [{"box_index": x1, "route_stop": route[x1]}, {"box_index": x1, "route_stop": route[x1]}],
 }
 
@@ -192,65 +183,5 @@ JSON:
 - Get route and box dropoff locations from the network JSON
 - Scan shelf for physical box locations
 - 
-
-************ This entire idea is being shelved for now.  I don't want to get rid of it, but I don't need it ************
-
-// Real connections with weights Vectors
-    // Nodes are 1 .. 10 inclusive
-    let mut connections:Vec<structs::ConnectionStruct> = vec![];
-
-    {   //////////////////////// Populate nodemap with connections //////////////////////
-        
-        let temp_connections = 
-        [
-            [1,2,2],
-            [1,3,1],
-            [3,2,2],
-            [2,8,3],
-            [2,7,5],
-            [2,6,4],
-            [2,4,4],
-            [4,6,1],
-            [7,6,2],
-            [4,5,2],
-            [8,7,3],
-            [8,9,6],
-            [9,10,1]
-        ];
-        
-        for i in 0 .. 13
-        {
-            connections.push(structs::ConnectionStruct{
-                source_node: temp_connections[i][0],
-                destination_node: temp_connections[i][1],
-                weight: temp_connections[i][2]
-            });
-        }
-
-    }
-
-    println!("Source Node\t->\tDestination Node");
-    for i in 0 .. connections.len()
-    {
-        println!("\t{}\t->\t{}", connections[i].source_node, connections[i].destination_node);
-    }
-
-    let route = [1,3,2,4,5,4,6,7,8,9,10];
-
-    // I'm working on this project on two different machines so I'm passing the example JSON back and forth here
-    // Yes I could include the python file in the repo. I don't know why I don't
-    {"route": [
-	    {"destination_id": 201, "x": 0, "y": 0}, 
-	    {"destination_id": 202, "x": 10, "y": 0},
-	    {"destination_id": 203, "x": 0, "y": 10},
-	    {"destination_id": 204, "x": 10, "y": 10},
-	    {"destination_id": 205, "x": 20, "y": 10}],
-    "box_loc": [
-	    {"tracking_number": 101, "destination": 0},
-	    {"tracking_number": 102, "destination": 1},
-	    {"tracking_number": 103, "destination": 1},
-	    {"tracking_number": 104, "destination": 2},
-	    {"tracking_number": 105, "destination": 3},
-	    {"tracking_number": 106, "destination": 3}]}
 
 */
